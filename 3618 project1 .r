e74ccc859ba95a133436a61217d26eeadbde4a38
yutong-ren <ren.563@osu.edu>
2019-10-31 20:53:23 -0400

    Initial commit

---
+++ 3618 project1 .r
@@ -0,0 +1,6 @@
+r1 <- c(NA, NA, NA, NA, NA, NA)
+r2 <- c(NA, NA, NA, NA, NA, NA)
+m1 <- matrix(data= c(r1, r2), nrow=2, ncol=6)
+rownames(m1) <- c("Halloween monsters", "Halloween monsters")
+colnames(m1) <- c("types of candy", "types of candy", "types of candy", "types of candy", "types of candy", "types of candy")
+ 
